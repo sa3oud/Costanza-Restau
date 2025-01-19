140772763321c33609d1ddcf8f0ba63f4e9bd1f9
Sa3oud <sahmad01@outlook.fr>
2025-01-19 11:17:14 -0800

    Configure GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Enable static export
+  basePath: '/Costanza-Restau', // Repository name
+  assetPrefix: '/Costanza-Restau', // Same as basePath
+}
+
+module.exports = nextConfig
